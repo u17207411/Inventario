d95b764ac7c46b0df323265e17c6538ed61724c0
u17207411 <68168968+u17207411@users.noreply.github.com>
2021-10-28 01:14:23 -0500

    borramos productodao

--- src/main/java/pe/com/sistema/dao/ProductoDao.java
+++
@@ -1,8 +0,0 @@
-package pe.com.sistema.dao;
-
-import pe.com.sistema.domain.Producto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductoDao extends JpaRepository<Producto, Integer>{
-    
-}
